7d2e8189c891fc301d11bd00d9552dad7a2dd096
BrosiahChill <josiahdeal93@gmail.com>
2026-07-15 15:02:56 -0500

    added chirps schema to db

--- main.go
+++ main.go
@@ -1,21 +1,52 @@
 package main
 
 import (
+	"chirpy/internal/database"
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
-	fileserverHits atomic.Int32
+	fileserverHits 	atomic.Int32
+	db      		*database.Queries
+	platform		string
 }
 
+
 func main() {
+	godotenv.Load()
+	
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		log.Fatal("PLATFORM must be set")
+	}
+
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error connecting to database. %s", err)
+		
+	}
+
+	dbQueries := database.New(dbConn)
+
 	const filepathRoot = "."
 	const port = "8080"
 
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		db:      		dbQueries,
+		platform: 		platform,
 	}
 
 	mux := http.NewServeMux()
@@ -23,7 +54,8 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
-	mux.HandleFunc("POST /api/validate_chirp", chirpHandler)
+	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
+	mux.HandleFunc("POST /api/users", apiCfg.handleUsersCreate)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
